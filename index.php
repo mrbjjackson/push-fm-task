7dfc564dccb91be9a156f7f38d49c8e58c82bd72
Ben Jackson <ben@mrbjjackson.com>
2019-12-02 15:10:11 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!-- saved from url=(0031)http://localhost/privacy-policy -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="automated digital marketing, music makers, content creators, cross-platform">
+<meta name="description" content="Push.fm is a marketing and promotional platform for digital content creators, helping them to build a strong fanbase. Create smart fanlinks, download gates and presaves with detailed analytics and meaningful insights.">
+<meta name="author" content="PUSH">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="v8g5bIr4Fd2QwsNd4sRY0IpYtoI3dzRA4pPzWW3e">
+<meta property="og:url" content="https://push.fm">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Push.fm">
+<meta property="og:image" content="https://d29xpkeqnsbaz3.cloudfront.net/43c6033a-6f4b-437d-9fce-c4876b2bae3c/images/banner.jpg">
+<meta property="og:description" content="Push.fm is a marketing and promotional platform for digital content creators, helping them to build a strong fanbase. Create smart fanlinks, download gates and presaves with detailed analytics and meaningful insights.">
+<title>Ben Jackson - Push FM task</title>
+<link rel="icon" type="image/png" href="https://d29xpkeqnsbaz3.cloudfront.net/43c6033a-6f4b-437d-9fce-c4876b2bae3c/favicon.png">
+<link href="./resources/fonts.css" rel="stylesheet">
+<link href="./resources/app.css" rel="stylesheet">
+<link href="./resources/downloadedstyles.css" rel="stylesheet">
+
+</head>
+
+<body class="overflow-x-hidden">
+<div class="overflow-visible sidebar-closed">
+
+<?php 
+include('header.php');
+include('aside.php');
+?>
+
+<main class="mt-16 lg:mt-20 overflow-y-visible overflow-x-hidden lg:overflow-x-visible ml-menu-sm p-8" id="app"></main>
+
+
+</body></html>
+
+
+
+
+
+
+
+<!-- <main class="mt-16 lg:mt-20 overflow-y-visible overflow-x-hidden lg:overflow-x-visible ml-menu-sm p-8">
+  <div class="generic-page">
+    <h3 class="lg:mt-8 pb-8 lg:pb-12 w-full border-b border-indigo">Popo</h3>
+    <div class="container mx-auto"><div>
+      <p><strong>Privacy </strong></p>
+<p>This privacy policy applies to  <a href="https://www.push.fm/">https://www.push.fm</a>, and details what information PUSH collects, how it is used and how to opt out of non-compulsory aspects of information gathering. PUSH will not disclose your personal information to third parties without your consent. PUSH always uses secure methods when handling your sensitive information. If you need any further help, please see “How to contact us” below</p>
+<p><strong>Information</strong></p>
+<p>At PUSH, we want to give you the best possible experience to ensure that you enjoy our service today, tomorrow, and in the future. Your privacy and the security of your personal data is, and will always be, enormously important to us. So, we want to transparently explain how and why we gather, store, share and use your personal data - as well as outline the controls and choices you have around when and how you choose to share your personal data.</p>
+<blockquote>
+<ol>
+<li>When you sign up for the PUSH Service - when you sign up to the PUSH Service, we collect certain personal data so you can use the PUSH Service such as your email address, name and locale.</li>
+</ol>
+</blockquote>
+
+<p><strong>Keeping your personal data safe</strong></p>
+<p>We are committed to protecting our users’ personal data. We implement appropriate technical and organisational measures to help protect the security of your personal data; however, please note that no system is ever completely secure.</p>
+<p>Your password protects your user account, so we encourage you to use a unique and strong password, limit access to your computer and browser, and log out after having used the PUSH Service.</p>
+<p><strong>Changes to this Privacy Policy</strong></p>
+<p>We may occasionally make changes to this Policy.</p>
+<p>When we make material changes to this Policy, we’ll provide you with prominent notice as appropriate under the circumstances, e.g., by displaying a prominent notice within the PUSH Service or by sending you an email. We may notify you in advance.</p>
+<p>Please, therefore, make sure you read any such notice carefully.</p>
+<p><strong>How to contact us</strong></p>
+<p>Thank you for reading our Privacy Policy. If you have any questions about this Policy, please contact our Data Protection Officer by using the ‘Contact Support’ form or by writing to us at the following address:</p>
+<p><a href="http://push.fm/">PUSH.fm</a></p>
+<p>Kernow House</p>
+<p>Gas Hill</p>
+<p>Truro, Cornwall, TR1 2XP</p>
+<p>United Kingdom</p>
+<p>We hope you enjoy PUSH</p>
+<p>© RouteNote Ltd.</p>
+<p>This Privacy Policy is effective as of 1st March 2019.</p>
+</div>
+</main> -->
